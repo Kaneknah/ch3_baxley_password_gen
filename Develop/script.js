dfee5be0b27031416a2d69c36ff773737639f18e
Kaneknah <jamesnbaxley@protonmail.com>
2022-11-07 08:44:28 -0700

    Java Functions and Lets created.

--- Develop/script.js
+++ Develop/script.js
@@ -1,66 +1,52 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var upperleters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-// Write password to the #password input
-
-function generatePassword() {
-  //window password
-
-  var passwordLength = input()
-  //window password length 8-128 (then validate console.log?)
-  if (password.length)
-}
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-}
-// window alert with generated password
 
-// Add event listener to generate button
+}
+
+function generatePassword() {
+
+  // var upperleters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  // var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  // var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="]
+  // var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+  let option = [["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="],
+  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]]
+
+  let password = ""
+  let passwordLength = window.prompt("What do you want the Password length to be?")
+  // Need for restriction on password length
+  let includeSpacialChars = window.confirm("Do you want Special character?")
+  let includeUpperCase = window.confirm("Do you want Upper case letters?")
+  let inclusedLowerCase = window.confirm("Do you want lower case letters?")
+  let includeNumbers = window.confirm("Do you want Numbers?")
+  //Need window.alter for restiction
+
+  for (let i = 0; i < passwordLength; i++) {
+    let optionSelector = i % option.length
+    //Need to be able to handle false Responses
+    let randNum = Math.floor(Math.random() * option[optionSelector].length);
+    password += option[optionSelector][randNum]
+
+
+  }
+
+  return password;
+}
+
+//generate a random number that will be our factor for special chars Must be less that our password.lenth 
+//repeat for each restirction
+
+//create a loop that goes from 0 to passwordLenth
+//for each loop, check if you are at a factor of one of your random numbers above
+//if you are, generate a new random number that is between 0 and the length of the targeted array
+//then go to the targeted array and and grab the ith character (ith the second random number generated)
+//add that character to our paassword variable above
+
 generateBtn.addEventListener("click", writePassword);
-
-// click Red Button
-//Prompt = Length "Please Enter the length you want your passwrod to be. Must be between 8 and 128 characters."\
-var input = window.prompt("How long would you like your Password to be?")
-//if number entered move on
-
-if (input === number) && (inputlength > 8 && < 128)
-//if blank, not a number, or two low or high - alert and loop "must enter a number between 8 and 128"
-if (input !== number) || (inputlength < 8 || > 128)
-window.alert("Sorry, You must enter only a Number between 8 and 128 characters")
-
-
-var wrongInput = window.alert("Sorry, You must enter only a Number between 8 and 128 characters")
-// if correct - log and move on (push)
-// Confirm = Want Upper Case "Would you like your password to include Upper Case Letters?""
-var inputLower = window.confirm("Would you like your password to include Uper Case Letters in your Password?")
-upperleters.push()
-//true - log and move on
-//false - Log and move on
-// confirm = Want Lower Case "would you like your password to include Lower Case Latters?"
-var inputLower = window.confirm("Would you like your password to include Lower Case Letters in your Password?")
-lowerLetters.push()
-//true - log and move on
-//false - Log and move on
-// confirm = Want symbols Case "Would you like your password to include Symbols?"
-var inputSymbol = window.confirm("Would you like your password to include Symbols in your Password?")
-if (inputSymbol) {
-  symbols.push()
-
-}
-//true - log and move on
-//false - Log and move on
-// confirm = Want numbers  "would you like your password to include nubers?""
-var inputNumbers = window.confirm("Would you like your password to include Numbers in your Password?")
-numbers.push
-  //true - log and move on
-  //false - Log and move on
-
-  //capture all logs (push?) and create a new array and display that array in a alert.
