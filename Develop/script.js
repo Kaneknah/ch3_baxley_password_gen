2a6f43568186f401cf1a3feee903b71c0c7ef526
Kaneknah <jamesnbaxley@protonmail.com>
2022-11-12 12:33:25 -0700

    Final JS updates for For Loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,68 +1,52 @@
-//seting out needed variables
+
 var generateBtn = document.querySelector("#generate");
-// this first attempt did not allow for the option to not select a option
-//var upperleters = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
-//var lowerLetters = "abcdefghijklmnopqrstuvwxyz"
-//var specialChars = "!@#$%^&*_-+="
-//var numbers = "0123456789"
-
-
-
-
 var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="]
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerLeters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
 
 //Function for creating the password from user inputs
 function generatePassword() {
-  let password = ""
-  let passwordLength = window.prompt("What do you want the Password length to be?");
+  var finalPassword = "";
+  var passwordLength = window.prompt("What do you want the Password length to be?");
 
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    window.alert("Please pick a Number between between 8 and 128")
+    window.alert("Please pick a Number between between 8 and 128");
     return
   }
-  let includeSpacialChars = window.confirm("Do you want Special character? Confirm ")
-  let includeUpperCase = window.confirm("Do you want Upper case letters?")
-  let inclusedLowerCase = window.confirm("Do you want lower case letters?")
-  let includeNumbers = window.confirm("Do you want Numbers?")
 
-  var optionsSelector = []
+  let includeSpacialChars = window.confirm("Do you want Special character? Confirm ");
+  let includeUpperCase = window.confirm("Do you want Upper case letters?");
+  let inclusedLowerCase = window.confirm("Do you want lower case letters?");
+  let includeNumbers = window.confirm("Do you want Numbers?");
+  var optionsSelector = [];
 
-  if (includeSpacialChars === true) {
-    optionsSelector.push(includeSpacialChars)
+  if (includeSpacialChars) {
+    optionsSelector.push(specialChars)
   }
-  if (includeUpperCase === true) {
-    optionsSelector.push(includeUpperCase)
+  if (includeUpperCase) {
+    optionsSelector.push(upperLetters)
   }
-  if (inclusedLowerCase === true) {
-    optionsSelector.push(inclusedLowerCase)
+  if (inclusedLowerCase) {
+    optionsSelector.push(lowerLetters)
   }
-  if (includeNumbers === true) {
-    optionsSelector.push(includeNumbers)
+  if (includeNumbers) {
+    optionsSelector.push(numbers)
   }
+  if(optionsSelector.length < 1 ) {
+    window.alert("Please choise at least one of the options provided");
+    return;
+  }
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randArr = optionsSelector[Math.floor(Math.random() * optionsSelector.length)];
+    var randChar = randArr[Math.floor(Math.random() * randArr.length)];
+    finalPassword += randChar;
+  } 
+
+  return finalPassword;
 }
-//Need window.alter for restiction
 
-// for (let i = 0; i < passwordLength; i++) {
-// let optionSelector = i % option.length
-//Need to be able to handle false Responses
-// let randNum = Math.floor(Math.random() * option[optionSelector].length);
-// password += option[optionSelector][randNum]
-//}
-//return password;
-
-var character = function (password, passwordLength, checking, string, range) {
-  if (checking === true && password.length < passwordLength) {
-    var randomChar = Math.floor(Math.random() * range);
-    password += Array(randomChar);
-
-
-
-  } return password
-}
 
 // Takes the output of the fucntion generate password and turns it into texts and connects it to the HTML location #password
 function writePassword() {
