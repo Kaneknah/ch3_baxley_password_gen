7d53d6e724ff2b74e260ca7aa8711f87b528ad7b
Kaneknah <jamesnbaxley@protonmail.com>
2022-11-14 09:16:55 -0700

    Updated JS Comments and README

--- Develop/script.js
+++ Develop/script.js
@@ -8,19 +8,21 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 //Function for creating the password from user inputs
 function generatePassword() {
   var finalPassword = "";
-  var passwordLength = window.prompt("What do you want the Password length to be?");
+  var passwordLength = window.prompt("What do you want the Password length to be? Please");
 
+//Sets the parameters for the password requirements and provides an alert if they are not met.
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     window.alert("Please pick a Number between between 8 and 128");
     return
   }
 
-  let includeSpacialChars = window.confirm("Do you want Special character? Confirm ");
-  let includeUpperCase = window.confirm("Do you want Upper case letters?");
-  let inclusedLowerCase = window.confirm("Do you want lower case letters?");
-  let includeNumbers = window.confirm("Do you want Numbers?");
+  let includeSpacialChars = window.confirm("Do you want Special characters in your password? Press OK for YES and CANCEL for NO.");
+  let includeUpperCase = window.confirm("Do you want Upper Case Letters in your password? Press OK for YES and CANCEL for NO.");
+  let inclusedLowerCase = window.confirm("Do you want Lower Case Letters in your password? Press OK for YES and CANCEL for NO.");
+  let includeNumbers = window.confirm("Do you want Numbers in your password? Press OK for YES and CANCEL for NO.");
   var optionsSelector = [];
 
+// Pushes the arrays selected by the user to be used in the final password. Also selects the alert if no arrays are included.
   if (includeSpacialChars) {
     optionsSelector.push(specialChars)
   }
@@ -37,18 +39,18 @@ function generatePassword() {
     window.alert("Please choise at least one of the options provided");
     return;
   }
-
+// Randomly selects a string from the selected arrays and adds it to the final password. repeating it till the selected length is obtained.
   for (var i = 0; i < passwordLength; i++) {
     var randArr = optionsSelector[Math.floor(Math.random() * optionsSelector.length)];
     var randChar = randArr[Math.floor(Math.random() * randArr.length)];
     finalPassword += randChar;
   } 
-
+// Returns the output of this function so that it may be used in the wrtiePassword function.
   return finalPassword;
 }
 
 
-// Takes the output of the fucntion generate password and turns it into texts and connects it to the HTML location #password
+// Takes the output of the function generate password and turns it into texts and connects it to the HTML location #password.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -56,14 +58,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-//generate a random number that will be our factor for special chars Must be less that our password.lenth 
-//repeat for each restirction
 
-//create a loop that goes from 0 to passwordLenth
-//for each loop, check if you are at a factor of one of your random numbers above
-//if you are, generate a new random number that is between 0 and the length of the targeted array
-//then go to the targeted array and and grab the ith character (ith the second random number generated)
-//add that character to our paassword variable above
-
-// Event starts the write password function
+// Event starts the write password function.
 generateBtn.addEventListener("click", writePassword);
